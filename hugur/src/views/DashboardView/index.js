719093e56878c99f31d35cf2c3f64140bc0a0861
BjarkiF <54940539+BjarkiF@users.noreply.github.com>
2022-03-16 19:07:02 +0000

    first commit

---
+++ hugur/src/views/DashboardView/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+const DashboardView = () => {
+    return (
+        <h2>Dashboard here</h2>
+    )
+};
+
+export default DashboardView;
