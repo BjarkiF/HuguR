0a61568efc36af73cbf6c7d141a64de63f9f932e
Natalia Potamianou <nataliapotamianou@Natalias-MacBook-Air.local>
2022-05-01 18:48:31 +0000

    added some styling

--- hugur/src/Components/DashCard/index.js
+++ hugur/src/Components/DashCard/index.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import { CardActionArea } from '@mui/material';
+import { CardActionArea, dividerClasses } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import CardMedia from '@mui/material/CardMedia';
 import { Link } from 'react-router-dom';
+import './DashCard.css'
 
 const DashCard = ({ card }) => {
     return (
-        <Card raised='true'>
-            <CardActionArea component={Link} to={card.path}>
-                <CardMedia 
-                    component="img"
-                    height="250"
-                    src={card.img}
-                    alt="Card Illustration"
-                />
-                <CardContent>
-                    <Typography variant="h5" component="div">
-                        {card.name}
-                    </Typography>
-                    <Typography variant="body2">
-                        {card.description}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+        /*<Card>
+             <CardActionArea component={Link} to={card.path}>
+                 <div className='C'>
+                     <CardMedia
+                         component="img"
+                         height="250"
+                         src={card.img}
+                         alt="Card Illustration"
+                     />
+                 </div>
+ 
+                 <CardContent>
+                     <Typography variant="h5" component="div">
+                         {card.name}
+                     </Typography>
+                     <Typography variant="body2">
+                         {card.description}
+                     </Typography>
+                 </CardContent>
+             </CardActionArea>
+         </Card> */
+        <div className='Card'>
+            <div className='CardName'>{card.name} </div>
+            <img className='ImgStyle' src={card.img} alt='CardImage' />
+            <div className='CardDescription'>{card.description} </div>
+       
+        </div>
+
     )
 }
 
--- hugur/src/Components/Footer/index.js
+++ hugur/src/Components/Footer/index.js
@@ -4,11 +4,14 @@ import React from 'react';
 
 const Footer = () => {
     return(
-        <Box sx={{ backgroundColor: '#E6F1D8', pt: 5}}>
-            <Typography variant="h5" component="div" sx={{mt: 5, mb: 5}}>
-                Footer placeholder
+        <Box sx={{ backgroundColor: '#D5E0C8', pt: 5}}>
+           
+        <Typography variant="h5" component="div" sx={{mt: 5, mb: 5, fontFamily:'dosis', fontSize: '12px'}}>
+            © HuguR Reykjavík University 2022
             </Typography>
+           
             
+    
         </Box>
     )
 }
