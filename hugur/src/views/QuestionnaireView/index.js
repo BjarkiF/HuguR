47963ac162d553b850d09ca242e4b874028c6c66
BjarkiF <54940539+BjarkiF@users.noreply.github.com>
2022-05-12 00:57:11 +0000

    fix database fetching

--- hugur/src/views/QuestionnaireView/index.js
+++ hugur/src/views/QuestionnaireView/index.js
@@ -1,31 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import Questionnaire from "../../Components/Questionnaire";
 import { db } from '../../utils/firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, onSnapshot } from 'firebase/firestore';
 
 const QuestionnaireView = () => {
     const [questions, setQuestions] = useState([]);
-    const [status, setStatus] = useState(false);
-
     useEffect(() => {
-        if (!status) {
-            fetchQuestions();
-            updateStatus(true);
-        }
-    });
-
-    const fetchQuestions = async () => {
-        const response = await getDocs(collection(db, "questionnaire"));
-
-        response.forEach((item)=>{
-            console.log(item);
-            setQuestions([...questions, item.data()]);
-        });
-    };
-
-    const updateStatus = (bool) => {
-        setStatus(bool);
-    }
+        onSnapshot(collection(db, "questionnaire"), (snapshot) => {
+            setQuestions(
+                snapshot.docs.map(doc => {
+                    return { id: doc.id, ...doc.data() }
+                })
+            )
+        })
+    }, []);
 
     const qlist = questions.map(q => (
         <Questionnaire q={q} />
