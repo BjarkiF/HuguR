a5200e6b6497fd6096023d633e6accc601ecb196
Natalia Potamianou <nataliapotamianou@Natalias-MacBook-Air.local>
2022-05-16 20:49:41 +0000

    styles for sign in and sign up

---
+++ hugur/src/Components/LoginComponent/index.js
@@ -0,0 +1,42 @@
+import { Grid, Typography, TextField, Button, Box } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+const LoginComponent = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const login = (e) =>  {
+        e.preventDefault();
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
+            .then(() => {
+                navigate("/", { replace: true });
+            })
+        }
+
+    return (
+        <Grid sx={{ maxWidth:"66%", margin: 'auto', display: 'flex', flexFlow: 'column', justifyContent: 'center' }}>
+            <Typography variant="h3" sx={{  paddingBottom: 6}}>Sign In</Typography>
+            <Box component="form" noValidate autoComplete="off" onSubmit={login}>
+                <TextField label="Email" placeholder='Enter Email' fullWidth required onChange={(e) => setEmail(e.target.value)} />
+                <TextField sx={{  margin: '18px 0' }}  label="Password" placeholder='Enter Password' type="password" fullWidth required onChange={(e) => setPassword(e.target.value)}/>
+                <Button type='submit' color='success' variant="contained" fullWidth>Sign In</Button>
+            </Box>
+            <Typography sx={{  marginTop: '16px' }}variant="body1">
+                <Link to="#">
+                Forgot Password?
+                </Link>
+            </Typography>
+            <Typography sx={{  margin: '18px 0' }}  variant="body2">
+                <Link to="/register">
+                Don't have an account? Sign up.
+                </Link>
+            </Typography>
+        </Grid>
+    )
+}
+
+export default LoginComponent;
---
+++ hugur/src/Components/RegisterComponent/index.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { Grid, Button, Typography, TextField, Box } from '@mui/material';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { useNavigate } from "react-router-dom";
+
+const RegisterComponent = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passConfirm, setPassConfirm] = useState("");
+    const [name, setName] = useState("");
+    const [error, setError] = useState("");
+    let navigate = useNavigate();
+
+    const validatePassword = ()  => {
+        let isValid = false;
+        if(email !== '' && name !== '' && password !== '' && passConfirm !== '') {
+            if (password === passConfirm) {
+                isValid = true;
+            } else {
+                setError("Passwords do not match.")
+            }
+        } else {
+            setError("Please fill in all fields")
+        }
+        return isValid;
+    }
+
+    const register = (e) =>  {
+        e.preventDefault();
+        const auth = getAuth();
+        if(validatePassword()) {
+            createUserWithEmailAndPassword(auth, email, password)
+                .then(async () => {
+                    await updateProfile(auth.currentUser, {
+                        displayName: name,
+                    }).then(() => {
+                        navigate("/", { replace: true })
+                    })
+                })  
+        }
+    }
+
+    return (
+        <Grid sx={{ maxWidth:"66%", margin: 'auto', display: 'flex', flexFlow: 'column', justifyContent: 'center' }}>
+        <Typography variant="h3" sx={{  paddingBottom: 6}}>Sign Up</Typography>
+        { error && <Typography variant="body1" sx={{ color: "red" }}>{error}</Typography>}
+        <Box component="form" noValidate autoComplete="off" onSubmit={register}>
+            <TextField label="Email" placeholder='Enter Email' fullWidth required onChange={(e) => setEmail(e.target.value)} />
+            <TextField sx={{  margin: '18px 0' }} label="Password" placeholder='Enter Password' type="password" fullWidth required onChange={(e) => setPassword(e.target.value)}/>
+            <TextField label="Confirm Password" placeholder='Confirm Password' type="password" fullWidth required onChange={(e) => setPassConfirm(e.target.value)}/>
+            <TextField sx={{  margin: '18px 0' }} label="Name" placeholder="Name" fullWidth required onChange={(e) => setName(e.target.value)}/>
+            <Button type='submit' color='success' variant="contained" fullWidth>Sign Up</Button>
+        </Box>
+        </Grid>
+    )
+};
+
+export default RegisterComponent;
