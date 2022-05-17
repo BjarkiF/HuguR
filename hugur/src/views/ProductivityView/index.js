5bff87aa991de1474b3c11175149ba0336340106
BjarkiF <54940539+BjarkiF@users.noreply.github.com>
2022-05-17 01:09:02 +0000

    ok

--- hugur/src/views/ProductivityView/index.js
+++ hugur/src/views/ProductivityView/index.js
@@ -21,7 +21,6 @@ const ProductivityView = () => {
 
     // Idea here that get_url returns whatever url we need for the first part of url
     const fieldList = fields.map(field =>
-        // <Link to={`/${get_url(field)}/${category}`}>
         <Link className='SectionLink' to={`/${field.trim().toLowerCase()}/${category}`}>
             <div className='field' onClick={console.log('hello')}>
                 <p>{field}</p>
